ae6904f849793dc9cdb3ea9de2252c2bae644c88
luizaugustovb <luizaugustovb@github.com>
2026-04-01 14:33:32 -0300

    fix: remove flash duplicado e corrige abertura do modal mais paginas

--- resources/views/pedidos/escanear.blade.php
+++ resources/views/pedidos/escanear.blade.php
@@ -4,29 +4,6 @@
 
 @section('page-content')
 <div x-data="{ showConfirmEnviar: false }" class="max-w-5xl mx-auto space-y-6">
-    <!-- Mensagens de Sucesso/Erro -->
-    @if(session('success'))
-    <div class="bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg p-4">
-        <div class="flex items-center">
-            <svg class="w-6 h-6 text-green-600 dark:text-green-400 mr-3" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-            </svg>
-            <p class="text-sm font-medium text-green-800 dark:text-green-200">{{ session('success') }}</p>
-        </div>
-    </div>
-    @endif
-
-    @if(session('error'))
-    <div class="bg-red-50 dark:bg-red-900/20 border border-red-200 dark:border-red-800 rounded-lg p-4">
-        <div class="flex items-center">
-            <svg class="w-6 h-6 text-red-600 dark:text-red-400 mr-3" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
-            </svg>
-            <p class="text-sm font-medium text-red-800 dark:text-red-200">{{ session('error') }}</p>
-        </div>
-    </div>
-    @endif
-
     @if($errors->any())
     <div class="bg-red-50 dark:bg-red-900/20 border border-red-200 dark:border-red-800 rounded-lg p-4">
         <div class="flex items-start">
@@ -667,9 +644,10 @@
                     const g = params.get('grupo');
                     if (g) {
                         this.grupoMaisPaginas = decodeURIComponent(g);
-                        this.modalMaisPaginas = true;
-                        // Limpar param da URL sem recarregar
                         window.history.replaceState({}, '', window.location.pathname + '#outros-documentos');
+                        this.$nextTick(() => {
+                            this.modalMaisPaginas = true;
+                        });
                     }
                 }
             },
